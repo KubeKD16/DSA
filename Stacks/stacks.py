de0b54344f0aa48cbff93ebf0958c39d6343b62a
kedar-pandhare <79280270+KubeKD16@users.noreply.github.com>
2021-06-17 23:07:29 -0400

    Update stacks.py

--- Stacks/stacks.py
+++ Stacks/stacks.py
@@ -4,8 +4,7 @@ class Stack:
         self.items = []
 
     def push(self, item):
-        """ Accepts item as parameter and appends it to the end of the list
-        Returns nothing
+        """ Accepts item as parameter and appends it to the end of the list.Returns nothing
 
         Runtime for this method is O(1) or constant time as appending to the end of the list
         happens in a constant time
@@ -13,16 +12,36 @@ class Stack:
         self.items.append(item)
 
     def pop(self):
-        pass
+        """Returns and Removes the last item from the list which is also the top item of the Stack.
+
+        Runtime is also constant time O(1) as we are indexing the last item of the list
+        """
+        if self.items:
+            return self.items.pop()
+
+        return None
 
     def peek(self):
-        pass
+        """ This method returns the last item in the list, which also happens to be the top item in the Stack
+
+        Runtime is constant O(1) as the indexing into a list is done in constant time """
+
+        if self.items:
+            return self.items[-1]
+
+        return None
 
     def size(self):
-        pass
+        """ Returns the length of the list that represents the stack
+        Runtime is constant time O(1) because finding the length of the list also happens in constant time"""
+
+        return len(self.items)
 
     def is_empty(self):
-        pass
+        """ Returns a boolean value describing whether or not the stack is Empty
+         The test of equality below happens in constant time O(1)
+        """
+        return self.items == []
 
     # def __str__(self):
     #     return f'{self.items}'
