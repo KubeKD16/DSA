2ba983f90e1c1112382794861916ec31c4c9f2b5
kedar-pandhare <79280270+KubeKD16@users.noreply.github.com>
2021-07-02 14:38:48 -0400

    Update moveZerosToRight.py

--- Arrays/moveZerosToRight.py
+++ Arrays/moveZerosToRight.py
@@ -1,19 +1,32 @@
-def zerotoRight(a):
-    if a == None and len(a) == 0: return False
+def zerotoRight(nums):
+    if nums == None and len(nums) == 0: return False
     i = 0
-    length = len(a)
+    j = 1
+    length = len(nums)
+    zeros = 0
 
-    while i < len(a):
-        if a[i] == 0:
-            a.pop(i)
-            a.append(0)
+    while i < len(nums):
+        if nums[i] == 0:
+            zeros += 1
+            # if j < len(nums) and nums[j] == 0:
+            #     nums.pop(j)
+            #     j = j+1
+
+            nums.pop(i)
+            # nums.append(0)
             length -= 1
-        else:
-            i +=1
+            i -= 1
 
-    return a
+        # else: i += 1
 
-s = [0,1,0,3,12]
-s1=[1,0,1]
+        i += 1
+
+    for j in range(zeros):
+        nums.append(0)
+
+    return nums
+
+s = [0,1,0,3,12,99,8,0,0,5]
+s1=[0,0,1]
 # print(zerotoRight(s))
-print(zerotoRight(s1))
+print(zerotoRight(s))
