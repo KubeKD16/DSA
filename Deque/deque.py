d102e0d2facd82c35926bd44aef2388df5996017
kedar-pandhare <79280270+KubeKD16@users.noreply.github.com>
2021-07-17 19:20:48 -0400

    Update deque.py

--- Deque/deque.py
+++ Deque/deque.py
@@ -3,25 +3,51 @@ class Deque:
         self.items = []
 
     def add_front(self, item):
-        pass
+        """ inserting at the front of the list which we add at the 0th index of the list
+        Insertion takes linear time O(n) time as we insert always at the start of the list, all the
+        other items in the list need to shift 1 element right"""
+        self.items.insert(0,item)
 
     def add_rear(self, item):
-        pass
+        """ adds the item to the last index of the list. Normal append operation that adds the item
+        to the last index of the list. Insertion takes O(1) time as we always add the item to the last index
+        of the list
+        """
+        self.items.append(item)
 
     def remove_front(self):
-        pass
+        """ Remove and return the item at the 0th index of the list. The runtime is also in linear time O(n)
+        as the remaining elements need to shift left in the list
+        """
+        if self.items:
+            return self.items.pop(0)
+        return None
 
     def remove_rear(self):
-        pass
+        """ Removes and returns the last item (right-most) of the list which represents the
+        
+        rear of the deque. The runtime is O(1) time as it's a constant time
+        """
+        if self.items:
+            return self.items.pop()
+        return None
 
     def peek_front(self):
-        pass
+        """ returns the first item in the list which represents the front of the deque.
+        Constant runtime of O(1) as we are simply returning the first item from left of the list """
+        if self.items:
+            return self.items[0]
+        return None
 
     def peek_rear(self):
-        pass
+        """ returns the last item in the list which represents the rear of the deque. Constant runtime of
+        O(1) as we are simply returning the last value in the list from right """
+        if self.items:
+            return self.items[-1]
+        return None
 
     def size(self):
-        pass
+        return len(self.items)
 
     def is_empty(self):
-        pass
+        return self.items == []
